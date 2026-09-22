4935758fe0af6907466d893a61187245ecd44a17
piyushmaru22 <piyush.maru76@gmail.com>
2026-09-22 22:09:20 +0530

    Update server.py

--- server.py
+++ server.py
@@ -33,6 +33,6 @@ def handle_message(msg):
     send(packet, broadcast=True)
 
 if __name__ == '__main__':
-    print("[SYSTEM] DCN WebSocket Server Initialized on Port 5000")
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    print(f"[SYSTEM] DCN WebSocket Server Initialized on Port {port}")
+    socketio.run(app, host='0.0.0.0', port=port, allow\_unsafe\_werkzeug=True)
